3a540dd66b90b625923117e478facbff46db9494
ihateb335 <zhar.misha@gmail.com>
2026-05-09 20:22:30 +0300

    Add docstring to Task-1

--- src/fibonacci.py
+++ src/fibonacci.py
@@ -2,9 +2,30 @@ from collections.abc import Callable
 
 
 def caching_fibonacci() -> Callable[[int], int]:
+    """
+    A function that returns the nth Fibonacci number
+
+    Raises:
+        TypeError: n must be an integer
+
+    Returns:
+        Callable[[int], int]: A function that returns the nth Fibonacci number
+    """
     fibonacci_cache = [0, 1, 1]
 
     def fibonacci(n: int) -> int:
+        """
+        A function that returns the nth Fibonacci number
+
+        Args:
+            n (int): The index of the Fibonacci number to return
+
+        Raises:
+            TypeError: n must be an integer
+
+        Returns:
+            int: The nth Fibonacci number
+        """
         nonlocal fibonacci_cache
 
         if not isinstance(n, int):
--- tests/test_fibonacci.py
+++ tests/test_fibonacci.py
@@ -8,6 +8,16 @@ from src.fibonacci import caching_fibonacci
 
 
 def _profiler(func: Callable[[int], int], n: int):
+    """
+    A function that profiles the execution time of a function
+
+    Args:
+        func (Callable[[int], int]): A function to profile
+        n (int): The number of the Fibonacci number to return
+
+    Returns:
+        tuple[int, float]: The result of the function and the execution time
+    """
     start = time.perf_counter()
     result = func(n)
     end = time.perf_counter()
@@ -15,11 +25,20 @@ def _profiler(func: Callable[[int], int], n: int):
 
 
 def test_caching_fibonacci_signature():
+    """
+    Test the signature of the caching_fibonacci function
+    """
     assert_type(caching_fibonacci, Callable[[], Callable[[int], int]])
 
 
 @pytest.mark.parametrize("n", [1.5, "1", None, True, False, [1], {1: 1}, (1, 1)])
 def test_unforseentype_throws_error(n):
+    """
+    Test that the caching_fibonacci function raises a TypeError if the input is not an integer
+
+    Args:
+        n (int): The number of the Fibonacci number to return
+    """
     with pytest.raises(TypeError):
         caching_fibonacci()(n)
 
@@ -42,10 +61,20 @@ def test_unforseentype_throws_error(n):
     ],
 )
 def test_caching_fibonacci_returns_correct_value(n, expected):
+    """
+    Test that the caching_fibonacci function returns the correct value for the given input
+
+    Args:
+        n (int): The number of the Fibonacci number to return
+        expected (int): The expected result
+    """
     assert caching_fibonacci()(n) == expected
 
 
 def test_caching_fibonacci_does_caching():
+    """
+    Test that the caching_fibonacci function does caching
+    """
     fibonacci = caching_fibonacci()
     _, time = _profiler(fibonacci, 100)
     _, time2 = _profiler(fibonacci, 101)
